35037e4871f73acc7b88a3f1f4f2452687f208e4
AUREL1GANS <86870842+AUREL1GANS@users.noreply.github.com>
2022-06-13 15:26:19 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,10 +9,10 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //Ubah Disini
-global.ownerNumber = ['6285609233482', '0']
-global.ownerName = 'Fandyyy X All Creator'
-global.packname = 'Ichigo Kurosaki'
-global.author = 'Whatsapp Bot 2022'
+global.ownerNumber = ['6285158185567', '0']
+global.ownerName = 'ORG'
+global.packname = ' ORG '
+global.author = ' ORG '
 global.prefa = ['','!','.','#','!'] //Ilangin Prefix Yang '' Kalau Gamau No Prefix
 global.sessionName = 'sesi'
 
